dc4a9d58ff8067552ec92c324a2217785bbb98f8
Chihiro SHIMAMURA <chihiros92@suou.waseda.jp>
2023-05-10 00:32:45 +0900

    Modify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 
 st.title('Streamlit 超入門')
 st.write('DataFrame')
@@ -9,9 +9,9 @@ st.write('DataFrame')
 ##################################################################
 ## 表の表示 (https://docs.streamlit.io/library/api-reference/data)
 ##################################################################
-df = pd.DataFrame({
-    '1列目': [1, 2, 3, 4],
-    '2列目': [10, 20, 30, 40]
-})
+# df = pd.DataFrame({
+#     '1列目': [1, 2, 3, 4],
+#     '2列目': [10, 20, 30, 40]
+# })
 
-st.write(df)
+# st.write(df)
