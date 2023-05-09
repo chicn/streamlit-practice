ea849d4c0fa0894e88fd6a9563dc908370ccfd3e
Chihiro SHIMAMURA <chihiros92@suou.waseda.jp>
2023-05-10 00:20:03 +0900

    Add files

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Streamlit 超入門')
+st.write('DataFrame')
+
+
+##################################################################
+## 表の表示 (https://docs.streamlit.io/library/api-reference/data)
+##################################################################
+df = pd.DataFrame({
+    '1列目': [1, 2, 3, 4],
+    '2列目': [10, 20, 30, 40]
+})
+
+st.write(df)
